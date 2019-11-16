c00e4270a9d67319a522a3454be85b2ca7f8e5d4
Zhabskyi <poltavagaz@gmail.com>
2019-11-16 01:15:30 +0000

    Crate index.html, header and signin

---
+++ public/scripts/views_manager.js
@@ -0,0 +1,29 @@
+$(() => {
+
+  const $main = $('#main-content');
+
+  window.views_manager = {};
+
+  window.views_manager.show = function(item) {
+    // $newPropertyForm.detach();
+
+
+    switch (item) {
+
+     case 'signUp':
+      $signUpForm.appendTo($main);
+      break;
+      case 'error': {
+        const $error = $(`<p>${arguments[1]}</p>`);
+        $error.appendTo('body');
+        setTimeout(() => {
+          $error.remove();
+          views_manager.show('listings');
+        }, 2000);
+
+        break;
+      }
+    }
+  }
+
+});
