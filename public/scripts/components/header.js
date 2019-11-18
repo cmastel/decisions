2ce897b45184dbbdbcce990f7dd55b49a266e3d4
Zhabskyi <poltavagaz@gmail.com>
2019-11-18 07:43:16 +0000

    Changed navbar when user is login

--- public/scripts/components/header.js
+++ public/scripts/components/header.js
@@ -17,14 +17,13 @@ $(() => {
         <div class="nav-body">
             <ul class="nav-body-list">
               <li><a href="#">Home</a></li>
-              <li><a href="#">About</a></li>
             </ul>
         </div>
         <div class="nav-buttons">
           <button id ="register" type="button" class="nav-buttons-style">
             <span class="nav-buttons-style-font">Register</span>
           </button>
-          <button type="button" class="nav-buttons-style">
+          <button id="login" type="button" class="nav-buttons-style">
             <span class="nav-buttons-style-font">Login</span>
           </button>
         </div>
@@ -34,14 +33,23 @@ $(() => {
       `
     } else {
       userLinks = `
-      <nav id="page-header__user-links" class="page-header__user-links">
-        <ul>
-          <li class="home">🏠</li>
-          <li>${user.first_name}</li>
-          <li class="my_polls_button">My Listings</li>
-          <li class="logout_button">Log Out</li>
-        </ul>
-      </nav>
+      <nav class="nav">
+      <div class="nav-title">
+          <img class="nav-title-logo" src="./styles/images/Logo.png" />
+      </div>
+      <div class="nav-body">
+          <ul class="nav-body-list">
+             <li><a href="#">Home</a></li>
+             <li>${user.first_name}</li>
+             <li>${user.last_name}</li>
+          </ul>
+      </div>
+      <div class="nav-buttons">
+        <button id="logout" type="button" class="nav-buttons-style">
+          <span class="nav-buttons-style-font">Log out</span>
+        </button>
+      </div>
+    </nav>
       `
     }
 
