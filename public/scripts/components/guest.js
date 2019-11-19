e81a61d27d0d2243c28d3f7f309a4b24ab024ea0
Chris Mastel <chris.mastel@gmail.com>
2019-11-19 02:19:20 +0000

    refactor guest.js

--- public/scripts/components/guest.js
+++ public/scripts/components/guest.js
@@ -1,32 +1,39 @@
 $(() => {
 
-  const $guestURL = $(`
-  <h2>Poll Title</h2>
-  <h3>Question</h3>
-  <form id="guest-url-form" class="guest-url-form">
-        <div class="guest-url-form__field-wrapperr">
-          <input type="text" name="response_1" placeholder="What rank (1-4)?">
-        </div>
-        <div class="guest-url-form__field-wrapper">
-            <input type="text" name="response_2" placeholder="What rank (1-4)?">
-        </div>
-        <div class="guest-url-form__field-wrapper">
-            <input type="text" name="response_3" placeholder="What rank (1-4)?">
-        </div>
-        <div class="guest-url-form__field-wrapper">
-            <input type="text" name="response_4" placeholder="What rank (1-4)?">
-        </div>
-        <div class="guest-url-form__field-wrapper">
-            <button>Create New Poll</button>
-            <a id="guest-url-form__cancel" href="#">Cancel</a>
-        </div>
-      </form>
-  `);
+  const $guestURL = $('#guestURL');
+
+  function updateGuestURL(pollData) {
+    const pageInfo = $(`
+    <h2>Poll Title</h2>
+    <h3>Question</h3>
+    <form id="guest-url-form" class="guest-url-form">
+          <div class="guest-url-form__field-wrapperr">
+            <input type="text" name="response_1" placeholder="What rank (1-4)?">
+          </div>
+          <div class="guest-url-form__field-wrapper">
+              <input type="text" name="response_2" placeholder="What rank (1-4)?">
+          </div>
+          <div class="guest-url-form__field-wrapper">
+              <input type="text" name="response_3" placeholder="What rank (1-4)?">
+          </div>
+          <div class="guest-url-form__field-wrapper">
+              <input type="text" name="response_4" placeholder="What rank (1-4)?">
+          </div>
+          <div class="guest-url-form__field-wrapper">
+              <button>Create New Poll</button>
+              <a id="guest-url-form__cancel" href="#">Cancel</a>
+          </div>
+        </form>
+    `);
+
+    $guestURL.append(pageInfo);
+  }
+
   window.$guestURL = $guestURL;
 
   getGuestURL()
   .then(function( json ) {
-    guestURL(json);
+    updateGuestURL(json);
   })
 
   $guestURL.on('submit', function(event) {
