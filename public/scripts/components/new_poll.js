50150289ba748e2451379011279c660d7d46677e
Zhabskyi <poltavagaz@gmail.com>
2019-11-19 00:08:19 +0000

    new_poll file change?

--- public/scripts/components/new_poll.js
+++ public/scripts/components/new_poll.js
@@ -3,31 +3,24 @@ $(() => {
   const $newPoll = $(`
   <form id="new-polls-form" class="new-polls-form">
         <p>New Poll</p>
-
         <div class="new-poll-form__field-wrapper">
           <input type="text" name="poll_title" placeholder="Title">
         </div>
-
         <div class="new-poll-form__field-wrapper">
           <input type="text" name="poll_question" placeholder="What is your question?">
         </div>
-
         <div class="new-poll-form__field-wrapperr">
           <input type="text" name="response_1" placeholder="Your first possible answer">
         </div>
-
         <div class="new-poll-form__field-wrapper">
             <input type="text" name="response_2" placeholder="Your second possible answer">
         </div>
-
         <div class="new-poll-form__field-wrapper">
             <input type="text" name="response_3" placeholder="Your third possible answer">
         </div>
-
         <div class="new-poll-form__field-wrapper">
             <input type="text" name="response_4" placeholder="Your fourth possible answer">
         </div>
-
         <div class="new-poll-form__field-wrapper">
             <button>Create New Poll</button>
             <a id="new-poll-form__cancel" href="#">Cancel</a>
