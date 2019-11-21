f5a13776efcbc8d828bf055633c0f442aa3f66dc
Chris Mastel <chris.mastel@gmail.com>
2019-11-21 18:02:23 +0000

    add date default to poll schema

--- db/schema/05_polls.sql
+++ db/schema/05_polls.sql
@@ -5,7 +5,7 @@ CREATE TABLE polls (
   id SERIAL PRIMARY KEY NOT NULL,
   user_id INTEGER REFERENCES users(id),
   title VARCHAR(255) NOT NULL,
-  created_on DATE,
+  created_on DATE NOT NULL DEFAULT CURRENT_DATE,
   admin_url VARCHAR(255) NOT NULL,
   guest_url VARCHAR(255) NOT NULL
 );
